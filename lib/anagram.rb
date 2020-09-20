4ee75e303e52a3a6a74fecff3b2db7b501cb271f
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-20 02:12:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,20 @@
 class Anagram
-  attr_accessor
+  attr_accessor :diaper, :ba, :listen, :allergy
+  
+  def initialize() 
+    @diaper = diaper
+  end
+  
+  def initialize()
+    @ba= ba
+  end
+  
+  def initialize()
+    @listen=listen
+  end
+  
+  def initialize()
+    @allergy = allergy
+  end
+  
+  diaper = Anagram.new()
